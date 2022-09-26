857dcd6e2270a14187938abbb7991997719f219c
nda111 <nda111@naver.com>
2022-09-26 09:34:22 +0000

    Added: Setup, clear examples

---
+++ example/clear.py
@@ -0,0 +1,12 @@
+import os
+from tqdm.auto import tqdm
+
+here = os.path.abspath(f'{__file__}/..')
+
+files = set(file 
+    for file in os.listdir(here) 
+    if os.path.isfile(file)) - { 'setup.py', 'clear.py' }
+
+for filename in tqdm(files):
+    fullname = os.path.join(here, filename)
+    os.remove(fullname)
--- example/setup.py
+++ example/setup.py
@@ -1,11 +1,37 @@
 import os.path as osp
 import torch
-from torchvision.models import resnet18, ResNet18_Weights, vgg11_bn, VGG11_BN_Weights
+from torchvision import models as torchmodels
 
 here = osp.abspath(f'{__file__}/..')
 
-models = (resnet18, vgg11_bn)
-params = (ResNet18_Weights.IMAGENET1K_V1, VGG11_BN_Weights.IMAGENET1K_V1)
+models = (
+    torchmodels.resnet18, 
+    torchmodels.resnet152, 
+
+    torchmodels.vgg11,
+    torchmodels.vgg16,
+    torchmodels.vgg19,
+
+    torchmodels.vgg11_bn,
+    torchmodels.vgg16_bn,
+    torchmodels.vgg19_bn,
+
+    torchmodels.inception_v3,
+)
+params = (
+    torchmodels.ResNet18_Weights.IMAGENET1K_V1,
+    torchmodels.ResNet152_Weights.IMAGENET1K_V2,
+
+    torchmodels.VGG11_Weights.IMAGENET1K_V1,
+    torchmodels.VGG16_Weights.IMAGENET1K_V1,
+    torchmodels.VGG19_Weights.IMAGENET1K_V1,
+
+    torchmodels.VGG11_BN_Weights.IMAGENET1K_V1,
+    torchmodels.VGG16_BN_Weights.IMAGENET1K_V1,
+    torchmodels.VGG19_BN_Weights.IMAGENET1K_V1,
+
+    torchmodels.Inception_V3_Weights.IMAGENET1K_V1,
+)
 
 for model, param in zip(models, params):
     model_name = model.__name__
@@ -21,6 +47,3 @@ for model, param in zip(models, params):
     model = model(weights=param)
     state_dict = model.state_dict()
     torch.save(state_dict, osp.join(here, f'{model_name}_weights.pt'))
-    
-    
-
